2288460449fc65afaaf76d8cdfe55b5d078e4b7c
Miguel <miguel.pina84@gmail.com>
2026-06-22 16:55:57 -0400

    Initial commit

---
+++ src/main/java/com/home/ingestiontest/IngestionTestApplication.java
@@ -0,0 +1,13 @@
+package com.home.ingestiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/ingestiontest/IngestionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.ingestiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
